30b1dae605f991972f1d204f45eae4471ae1ace8
Richard Gomes <gomesrichard002@gmail.com>
2026-03-08 23:06:16 +0000

    criada a int_dimensao_datas

---
+++ models/intermediate/int_dimensao_datas.sql
@@ -0,0 +1,53 @@
+with date_spine as (
+
+    {{
+        dbt_utils.date_spine(
+            datepart = "day",
+            start_date = "cast('2000-01-01' as date)",
+            end_date = "cast('2030-01-01' as date)"
+        )
+    }}
+
+),
+
+criar_datas as (
+
+select
+    cast(date_day as date) as pk_data
+    , extract(day from date_day) as dia
+    , case extract(dow from date_day)
+        when 1 then 'domingo'
+        when 2 then 'segunda-feira'
+        when 3 then 'terça-feira'
+        when 4 then 'quarta-feira'
+        when 5 then 'quinta-feira'
+        when 6 then 'sexta-feira'
+        when 7 then 'sábado'
+      end as dia_semana_nome
+    , case extract(month from date_day)
+        when 1 then 'janeiro'
+        when 2 then 'fevereiro'
+        when 3 then 'março'
+        when 4 then 'abril'
+        when 5 then 'maio'
+        when 6 then 'junho'
+        when 7 then 'julho'
+        when 8 then 'agosto'
+        when 9 then 'setembro'
+        when 10 then 'outubro'
+        when 11 then 'novembro'
+        when 12 then 'dezembro'
+      end as mes
+    , extract(year from date_day) as ano
+    , concat('T', extract(quarter from date_day)) as trimestre_nome
+    , case 
+        when extract(dow from date_day) in (1,7) then true
+        else false
+      end as is_final_de_semana
+
+from date_spine
+
+)
+
+select *
+from criar_datas
